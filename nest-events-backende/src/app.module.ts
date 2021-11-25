89cde95c2bb60789eb9b247554f94b85a381aacf
Jhon Jairo Chaparro <wenikore@gmail.com>
2021-11-24 22:55:47 -0500

    mapeo de orm con attributos en mysql

--- nest-events-backende/src/app.module.ts
+++ nest-events-backende/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'172.18.0.4', //docker ip container
+      port: 3306,
+      username:'root',
+      password:'example',
+      database:'nest-events',
+      entities:[Event],
+      synchronize: true
+    })
+  ],
   controllers: [
     AppController,
     EventsController
--- nest-events-backende/src/event.entity.ts
+++ nest-events-backende/src/event.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('event',{name:'event'})
 export class Event {
+    @PrimaryGeneratedColumn()
     id:number
+    
+    @Column({length:100})
     name: string;
+
+    @Column()
     description: string;
+
+    @Column({name:'when_date'})
     when: Date;
+
+    @Column()
     address: string;
 }
