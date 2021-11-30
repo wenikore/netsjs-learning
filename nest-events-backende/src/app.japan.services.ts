d010e090e0f82e4e0f6c46e809aa1eaeb894e941
Jhon Jairo Chaparro <wenikore@gmail.com>
2021-11-29 22:22:18 -0500

    configuracion enviroment

--- nest-events-backende/src/app.japan.services.ts
+++ nest-events-backende/src/app.japan.services.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppJapanServices{
@@ -7,12 +8,15 @@ export class AppJapanServices{
     @Inject('APP_NAME')
     private readonly name:string,
     @Inject('MESSAGE')
-    private readonly message:string
+    private readonly message:string,
+    private configService: ConfigService,
   ){}
 
 
     getHello(): string {
+        console.log(process.env.HOLA)
         console.log(process.env.DB_HOST)
+        
         return `こんにちは世界! from ${this.name}, ${this.message}`;
     }
 
--- nest-events-backende/src/app.module.ts
+++ nest-events-backende/src/app.module.ts
@@ -2,26 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from './events/event.entity';
 import { EventsModule } from './events/events.module';
 import { AppJapanServices } from './app.japan.services';
 import { AppDummyServices } from './app.dummy';
 import { ConfigModule } from '@nestjs/config';
+import ormConfig from 'config/orm.config';
+import ormConfigProd from 'config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(
-      //envFilePath: ['.production.env', '.staging.env'],
-    ), //es importante configurar para ver las para enviroments
-    TypeOrmModule.forRoot({
-      type:'mysql',
-      host:process.env.DB_HOST, //docker ip container
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities:[Event], 
-      synchronize: true
+    ConfigModule.forRoot({
+      //envFilePath:'../config/production.env',
+      isGlobal:true,
+      load:[ormConfig],
+      expandVariables : true,
+      
+    }), //es importante configurar para ver las para enviroments
+    TypeOrmModule.forRootAsync({  //la db se cargara como factory que esta en un archivo 
+      useFactory: process.env.NODE_ENV != 'production' ? ormConfig :ormConfigProd
     }),
     EventsModule 
   ],
