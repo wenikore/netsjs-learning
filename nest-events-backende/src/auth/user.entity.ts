fb576c1cdbd9332d66c23f7a33965718658c1974
Jhon Jairo Chaparro <wenikore@gmail.com>
2021-12-05 00:07:38 -0500

    relaciones one to one

---
+++ nest-events-backende/src/auth/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Profile } from "./profile.entity";
+
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @OneToOne(() => Profile,{
+      cascade:true
+  })
+  @JoinColumn()
+  profile: Profile;
+}
